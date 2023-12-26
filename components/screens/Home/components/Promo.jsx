b0c052e41954619bd511f6e7b83cebfb648e5cb8
Boris Belmar <borisbelmarm@gmail.com>
2023-12-26 18:26:09 -0300

    Nueva promo

--- components/screens/Home/components/Promo.jsx
+++ components/screens/Home/components/Promo.jsx
@@ -3,13 +3,13 @@ import Image from 'next/image'
 export default function Promo() {
   return (
     <Image
-      src="/images/promo.jpg"
+      src="/images/slides/promo-1.jpg"
       alt="Promoción"
       width={1920}
       height={1080}
       className="w-full object-cover aspect-[2/1] md:aspect-auto"
       placeholder="blur"
-      blurDataURL="/images/promo.jpg"
+      blurDataURL="/images/slides/promo-1.jpg"
       priority
     />
   )
